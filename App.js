d64b1831b8adb85a2f4ff5c4263771692a35eaef
Av3001 <113854539+Av3001@users.noreply.github.com>
2023-08-13 14:15:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { generateQRCode } from './api';
+import './App.css'
+
+function QRCodeGenerator() {
+  const [qrData, setQrData] = useState('example.com');
+  const [qrSize, setQrSize] = useState('200x200');
+
+  return (
+    <div className="app-container">
+      <h1 className="app-title">QR Code Generator</h1>
+      <div className="input-container">
+        <label className="input-label">QR URL:</label>
+        <input
+          className="input-field"
+          type="text"
+          value={qrData}
+          onChange={(e) => setQrData(e.target.value)}
+        />
+      </div>
+      <div className="input-container">
+        <label className="input-label">QR Size:</label>
+        <input
+          className="input-field"
+          type="text"
+          value={qrSize}
+          onChange={(e) => setQrSize(e.target.value)}
+        />
+      </div>
+      <div className="qr-code">
+        <img className="qr-image" src={generateQRCode(qrData, qrSize)} alt="QR Code" />
+      </div>
+    </div>
+  );
+}
+
+export default QRCodeGenerator;
